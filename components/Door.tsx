6dcde11f4a421e745e46583d5161fef0b7f9a534
Celio Junior <celiocausjunior@gmail.com>
2022-11-29 15:40:10 -0300

    Door component final state

--- components/Door.tsx
+++ components/Door.tsx
@@ -3,25 +3,36 @@ import styles from '../styles/door.module.css'
 import React from 'react'
 import DoorModel from '../models/doorModel'
 
-interface DoorProps{
+interface DoorProps {
   value: DoorModel
-  onChange: (newDoor: DoorModel ) => void
+  onChange: (newDoor: DoorModel) => void
 }
 
-function Door(props:DoorProps) {
+function Door(props: DoorProps) {
   const door = props.value
-  const selectedDoor = door.selected ? styles.selectedDoor : ''
+  const selectedDoor = (door.selected && !door.isOpen) ? styles.selectedDoor : ''
+  const switchDoorState = e => props.onChange(door.switchSelectedDoor())
+  const openDoor = e => {
+    e.stopPropagation()
+    props.onChange(door.openDoor())
+  }
 
-  const switchState = e => props.onChange(door.switchSelectedDoor())
-  return (
-    <div className={styles.area} onClick={switchState}>
-      <div className={`${styles.frame} ${selectedDoor}`}>
-        <div className={styles.door}>Door
-          <div className={styles.numberDoor}>
-            {door.number}
-          </div>
-          <div className={styles.doorHandle} />
+  function doorRender() {
+    return (
+      <div className={styles.door}>Door
+        <div className={styles.numberDoor}>
+          {door.number}
         </div>
+        <div className={styles.doorHandle} onClick={openDoor} />
+      </div>
+    )
+  }
+
+
+  return (
+    <div className={styles.area} onClick={switchDoorState}>
+      <div className={`${styles.frame} ${selectedDoor}`}>
+        {door.isOpen ? null : doorRender()}
       </div>
       <div className={styles.floor} />
     </div>
